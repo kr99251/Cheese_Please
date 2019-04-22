870fd551457da2023bfe0ad9ed436df7a30a5b14
Kelly E Robinett <kr99251@srcs.loc>
2019-04-22 14:24:23 -0700

    moving attempt

--- cheese.js
+++ cheese.js
@@ -21,7 +21,7 @@ container.addEventListener("keydown", moveMouse);
 function moveMouse(event) {
     let xPosition = event.clientX
     if (event.keyCode === 37){
-        xPosition = xPosition - 5;
+        mouse.style.xpos = mouse.style.xpos -5;
     } else if (event.keyCode === 39) {
-        xPosition = xPosition + 5
+        mouse.style.xpos = mouse.style.xpos +5;
     }
