bf41d1f07db4e72c80c2bd1e90acd928a4a476ad
Kelly E Robinett <kr99251@srcs.loc>
2019-04-22 13:39:54 -0700

    new link

--- cheese.js
+++ cheese.js
@@ -15,7 +15,7 @@ var container = document.getElementById("container");
 
 let animatedDiv = document.createElement("img");
   animatedDiv.className = "mouse";
-  animatedDiv.src = "http://www.sclance.com/pngs/monkey-cartoon-png/monkey_cartoon_png_892923.png"
+  animatedDiv.src = "https://www.google.com/search?q=mouse+cartoon&safe=strict&rlz=1C1GGRV_enUS811US811&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjf15Xgw-ThAhXO-lQKHS48C4oQ_AUIDigB&biw=1920&bih=974#imgrc=njiARnAZlaP6xM:";
   animatedDiv.id = "mouse";
 container.appendChild(animatedDiv);
 
