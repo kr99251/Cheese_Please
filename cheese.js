ebe04701e3a96191000638a729995e082edb0487
Ana  Quach <aq64717@srcs.loc>
2019-04-26 13:29:04 -0700

    moving sideways actually works

--- cheese.js
+++ cheese.js
@@ -1,3 +1,46 @@
+
+class CheesePlease {
+    constructor() {
+        this.mouse = document.getElementById('mouse')
+        this.obstacles = [];
+    }
+
+    play() {
+        // this is where you will populate the array, update ...
+    }
+
+
+    moveLeft() {
+        this.mouse.style.left = parseInt(mouse.style.left) - 5 + 'px';
+    }
+
+    moveRight() {
+        this.mouse.style.left = parseInt(mouse.style.left) + 5 + 'px';
+    }
+
+    moveSideways(e) {
+
+
+
+    }
+
+
+}
+
+let game = new CheesePlease();
+
+window.addEventListener('keydown', (e) => {
+
+        if (e.keyCode == '37') {
+            game.moveLeft();
+        } else if (e.keyCode == '39') {
+            game.moveRight();
+        }
+});
+
+game.play();
+
+
 class Obstacle {
     constructor(_xpos, _ypos, _size, _id) {
     this.xpos = _xpos;
@@ -10,28 +53,4 @@ class Obstacle {
     this.elem.style.bottom = this.ypos + "px";
     this.elem.style.left = this.xpos + "px";
 }
-}
-
-var mouse = document.getElementById('mouse')
-document.onkeydown = moveSideways;
-
-function moveSideways(event) {
-
-    event = event || window.event;
-
-    if (e.keyCode == '32') {
-        rotate();
-    } else if (e.keyCode == '37') {
-        moveLeft();
-    } else if (e.keyCode == '39') {
-        moveRight();
-    }
-
-}
-function moveLeft() {
-    mouse.style.left = parseInt(mouse.style.left) - 5 + 'px';
-}
-
-function moveRight() {
-    mouse.style.left = parseInt(mouse.style.left) + 5 + 'px';
 }
